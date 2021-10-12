1d109923e73ef36fcf49940db6f378066bc13f87
Parsa Noori <parsanoori01@yahoo.com>
2021-10-12 23:31:15 +0330

    preprocessor minor change

--- preprocessor/src/preprocessor.cpp
+++ preprocessor/src/preprocessor.cpp
@@ -9,7 +9,7 @@ string preprocess(string i){
     i = regex_replace(i,regex("\r"),"\n");
 
     smatch m;
-    regex e("define\\s+([A-Za-z]\\w*)\\s+(.*)\n"); // find all defines
+    regex e("define\\s+([A-Za-z_]\\w*)\\s+(.*)\n"); // find all defines
     while(regex_search(i,m,e)) { // search for pattern
         string r = m[1].str(),t=m[2].str(); // store before it messes up
         i.erase(m.position(),m.position()+m[0].length()); // erase the shit
